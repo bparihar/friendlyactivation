0eb7a3ef27f1b10c1c4282d4608b17aaf8e17f1e
bparihar <bparihar93@gmail.com>
2026-06-06 13:20:27 +0530

    Update MAS_AIO.cmd

--- MAS_AIO.cmd
+++ MAS_AIO.cmd
@@ -1,30 +1,6 @@
-@::Please stop false-flagging this script over and over. This is the 3rd time I've had to update the script to deal with the false positives.
-@::Nothing in this script is malicious and it is used by thousands of people *daily*.
-@::Is a real person even the one deciding to flag this or is it Copilot told to flag anything it decides not to like?
-@::If a campaign is using this script as a cover, please flag parts of the campaign instead of the script. This isn't the first time we experienced collateral damage due to campaigns completely unrelated to us.
-@::If you disagree that MAS is not malicious, we would appreciate if you contacted us to explain what constitutes MAS as "Sonbokli" and we'll look into it. Thank you. 
-@set masver=3.11
+
 @setlocal DisableDelayedExpansion
 @echo off
-
-
-
-::  For command line switches, check m{}assgrave{dot}dev/command_line_switches
-::  If you want to better understand script, read from separate files version. 
-
-
-
-::============================================================================
-::
-::   Homepage: m{}assgrave{dot}dev
-::
-::============================================================================
-
-
-
-::========================================================================================================================================
-
-::  Set environment variables, it helps if they are misconfigured in the system
 
 setlocal EnableExtensions
 setlocal DisableDelayedExpansion

